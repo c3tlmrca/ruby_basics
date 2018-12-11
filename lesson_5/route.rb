bf0ec80ceed40bd3fb5285b2c5614cf3dcbc425c
Andrew Bush <c3tlmrca@gmail.com>
2018-12-11 23:17:16 +0300

    c3

--- lesson_5/route.rb
+++ lesson_5/route.rb
@@ -1,8 +1,12 @@
+require './instance_counter.rb'
+
 class Route
+  include InstanceCounter
   attr_reader :stations
 
   def initialize(stations)
     @stations = stations
+    register_instance
   end
 
   def add_station(station)
